984eb15035684392d99dee3abd9ddec97cc0fb75
Siddhanth Unnithan <sidunnithan@gmail.com>
2017-11-29 16:24:24 -0500

    Reorganized helper scripts.

--- shell_helpers/send-turn.sh
+++ shell_helpers/send-turn.sh
@@ -1,6 +1,6 @@
 # convenience script to execute send_turn command against client
 # usage: bash send-turn.sh <client_name> <card_index>
 
-docker exec -it $1 python /App/send-turn.py $1 $2
+docker exec -it $1 python /App/shell_helpers/send-turn.py $1 $2
 
 docker exec -it $1 curl localhost:8080/get_hand
